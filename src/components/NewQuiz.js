939eff5aab48d3fa74abdc9b632b317f3d38a8eb
Mark <mxpm02@uw.edu>
2024-06-02 12:41:55 -0700

    fixed question order on reset

--- src/components/NewQuiz.js
+++ src/components/NewQuiz.js
@@ -61,7 +61,7 @@ export function NewQuiz(props) {
     // I don't know how to connect this to the quizQuestions to roll a new set of players upon reset
     const resetGame = () => {
         setIndex(0);
-        setQuestion(quizQuestions[index]);
+        setQuestion(quizQuestions[0]);
         setScore(0);
         setLock(false);
         setFinal(false);
